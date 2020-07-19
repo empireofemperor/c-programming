4b3fe55355ab35db2d43047a247cb8b3a7b56bb4
Jatin sharma <jatinsharma089659@gmail.com>
2020-07-19 14:38:53 +0530

    name changed

--- check-armstrong(while-loop).c
+++ check-armstrong(while-loop).c
@@ -3,8 +3,11 @@
 
 /************************************************************************************************
 AIM    -    WAP to input the integer no. and check the no. is armstrong or not by using while loop
-
 OBJECT -     use of While-loop
+PROGRAMMER    - Jatin sharma
+
+EXPLANATION  -  Armstrong number is a number that is equal to the sum of cubes of its digits. For 
+		example 0, 1, 153, 370, 371 and 407 are the Armstrong numbers
 *************************************************************************************************/
 
 int main()
