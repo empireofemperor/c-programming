750bd979c4c96c4f115384a5f9b813ae412c62e3
Jatin sharma <jatinsharma089659@gmail.com>
2020-07-19 14:20:19 +0530

    name changed

--- switch-case-example(by-integer-input).c
+++ switch-case-example(by-integer-input).c
@@ -4,6 +4,7 @@
 /***************************************************************************************************
 AIM           -  WAP a program to using switch case statement for integer
 OBJECTIVE     -  use of only "switch" statement
+PROGRAMMER    - Jatin sharma
 ***************************************************************************************************/
 
 int main()
