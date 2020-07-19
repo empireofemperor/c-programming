461da7db37a9d340f6614a12632ad5218cb9d1fb
Jatin sharma <jatinsharma089659@gmail.com>
2020-07-19 14:45:35 +0530

    name changed + hint

--- function-with-arg-without-return-val.c
+++ function-with-arg-without-return-val.c
@@ -4,9 +4,13 @@
 /*****************************************************************************************************
 AIM      -   WAP to print the area of a circle by using functions with argument without return value
 OBJECT   -   use of function
+PROGRAMMER    - Jatin sharma
 *************************************************************************************************/
 
 void area(int r);    //function declaration
+
+//here "void" stands for without-return-value & "int r" stands for with argument.
+//which means printf-statement will be in main function.
 
 int main()
 {
