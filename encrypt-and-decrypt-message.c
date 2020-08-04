5b2a338b4b6c272060bee6fd2a8fdd217f43db16
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2020-08-04 14:06:51 +0530

    adding comments to understand the code easily

--- encrypt-and-decrypt-message.c
+++ encrypt-and-decrypt-message.c
@@ -1,28 +1,40 @@
 #include <stdio.h>
 #include <string.h>
 
+/************************************************************************************************
+ 
+AIM         -    Ask use to encrypt or decrypt message and take the message as input and covert
+                 into encrypted message or decrypted message.
+Programmer  -    Jatin Sharma
 
+************************************************************************************************/
+
+//three functions
 void encrypting_message(void);
 void decrypting_message(void);
 void thanks(void);
 
+
+//main functions
 int main()
 {
-
+      //declaration
       char choice[10];
       char encrypt[] = "encrypt";
       char encrypt2[] = "e";
 
+      //asking for the choice from user
       printf("This Program is for making message Encrypt and Decrypt.\nWhat do You wanna do : ");
       gets(choice);
 
+
       if (strcmp(choice, encrypt) == 0 || strcmp(choice, encrypt2) == 0)
       {
-            encrypting_message();
+            encrypting_message();  //function call by value
       }
       else
       {
-            decrypting_message();
+            decrypting_message(); //function call by value
       }
 
 
@@ -30,19 +42,24 @@ int main()
 }
 
 
-
+//declaratin of the encrpted function
 void encrypting_message(void)
 {
       char message[100];
       int i = 0;
 
+      //taking message to encrypt
       printf("Enter the message to Encrypt : ");
       gets(message);
+
+      //while loop
       while (message[i] != '\0')
       {
             message[i] = message[i] + 2;
             i++;
       }
+
+      //printing the encrypted message
       printf("Encrypted Message : %s", message);
       thanks();
 }
@@ -52,6 +69,7 @@ void decrypting_message(void)
       char message[100];
       int i = 0;
 
+      //taking message to decrypt
       printf("Enter the message to Decrypt : ");
       gets(message);
       while (message[i] != '\0')
@@ -59,10 +77,14 @@ void decrypting_message(void)
             message[i] = message[i] - 2;
             i++;
       }
+
+      //printing the decrypted message
       printf("Encrypted Message : %s", message);
       thanks();
 }
 
+
+//thanks function just for fun
 void thanks(void){
       printf("Thanks for using!........peace.!");
 }
